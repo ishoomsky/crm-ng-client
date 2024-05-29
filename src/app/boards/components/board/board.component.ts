459b6c287dafe29eb54d9a8089492fb5bd0b8398
ishumsk <ivan.shumski@ibm.com>
2024-05-29 14:30:04 +0300

    client: implement socket events leave, join

--- src/app/boards/components/board/board.component.ts
+++ src/app/boards/components/board/board.component.ts
@@ -1,7 +1,11 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { BoardsService } from "@app/boards/services/boards.service";
 import { BoardService } from "@app/boards/services/board.service";
 import { JsonPipe, NgIf } from "@angular/common";
+import { SocketService } from "@app/shared/services/socket.service";
+import SocketEvents from "@app/shared/types/socket-events.enum";
+import { NavigationStart, Router } from "@angular/router";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-board',
@@ -14,19 +18,34 @@ import { JsonPipe, NgIf } from "@angular/common";
   styleUrl: './board.component.scss',
   providers: [BoardsService, BoardService]
 })
-export class BoardComponent implements OnInit{
+export class BoardComponent implements OnInit {
   @Input() boardId!: string;
 
   private boardsService = inject(BoardsService);
+  private socketService = inject(SocketService);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
   public boardService = inject(BoardService);
   ngOnInit() {
-    if (!this.boardId) {
-      throw new Error('Cannot get board id from the url');
-    }
+    this.initializeListeners();
+    this.socketService.emit(SocketEvents.BoardsJoin, {boardId: this.boardId});
     this.fetchData();
   }
 
+  private initializeListeners(): void {
+    this.router.events
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.boardService.leaveBoard(this.boardId);
+      }
+    })
+  }
+
   public fetchData() {
+    if (!this.boardId) {
+      throw new Error('Cannot get board id from the url');
+    }
 
     this.boardsService.getBoard(this.boardId)
       .subscribe((board) => {
--- src/app/boards/services/board.service.ts
+++ src/app/boards/services/board.service.ts
@@ -1,7 +1,15 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { BoardInterface } from "@app/boards/types/board.interface";
+import { SocketService } from "@app/shared/services/socket.service";
+import SocketEvents from "@app/shared/types/socket-events.enum";
 
 @Injectable()
 export class BoardService {
+  private socketService = inject(SocketService);
   public board = signal<BoardInterface | null>(null);
+
+  public leaveBoard(boardId: string): void {
+    this.board.set(null);
+    this.socketService.emit(SocketEvents.BoardsLeave, {boardId});
+  }
 }
